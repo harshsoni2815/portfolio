cb4cbd6276bcf95c42b24829ff4ca701d79733bf
harsh soni <harshsoni0210m@gmail.com>
2024-04-28 18:36:13 +0530

    scrolling effect

--- js.js
+++ js.js
@@ -1,9 +1,24 @@
 let section = document.querySelectorAll('section');
 let navlink = document.querySelectorAll('header nav a');
 
-
+let a=0;
 window.onscroll = ()=>{
   section.forEach(sec=>{
-    console.log(window.scrollY);
-  })
+    let top = window.scrollY
+    let offset = sec.offsetTop-150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top>=offset && top< offset+height){
+      navlink.forEach(link=>{
+          link.classList.remove('active');
+          document.querySelector('header nav a[href*='+id+']').classList.add('active');
+      });
+    }
+  });
 };
+section.forEach(sec=>{
+  let top = window.scrollY
+  console.log("top:",top,"offset:",sec.offsetTop,"  offset hgt",sec.offsetHeight," id:", sec.getAttribute('id'));
+});
+
